dd96b8f7df3ea05e651e24e82636edf6b6743d65
Svyatoslav Shypka <svyat200909@gmail.com>
2023-10-08 20:25:52 +0200

    1-3-1

--- 1-0-java-basics/1-3-2-heterogeneous-max-holder/src/main/java/com/bobocode/basics/HeterogeneousMaxHolder.java
+++ 1-0-java-basics/1-3-2-heterogeneous-max-holder/src/main/java/com/bobocode/basics/HeterogeneousMaxHolder.java
@@ -17,7 +17,6 @@ import java.util.Map;
  */
 
 public class HeterogeneousMaxHolder {
-
     /**
      * A method put stores a provided value by its type, if the value is greater than the current maximum. In other words, the logic
      * of this method makes sure that only max value is stored and everything else is ignored.
